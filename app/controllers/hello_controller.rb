982b52a99f62df5439b9a0d626d649c6cb519f81
athan-tvaexe <athan@netprotections.co.jp>
2025-09-09 12:30:41 +0700

    Exercise 2: Create GET API with params and write test cases for testing API

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,4 +2,13 @@ class HelloController < ApplicationController
   def index
     render json: { message: "Hello, World!" }
   end
+
+  def greet
+    name = params[:name]
+    if name.blank?
+      render json: { error: "Name parameter is missing" }, status: :bad_request
+    else
+      render json: { greeting: "Hello, #{name}!" }
+    end
+  end
 end
